e887dc449886ba89eb477c825081910e5a10aec0
Roby <robyeka@email.unikom.ac.id>
2018-10-12 03:37:00 +0700

    menambahkan paket inheritance

---
+++ src/inheritance/BangunDatar.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inheritance;
+
+/**
+ *
+ * @author eka
+ */
+public class BangunDatar {
+    
+    float luas() {
+        System.out.println("menghitung luas bangundatar");
+        return 0;
+    }
+    
+    float keliling() {
+        System.out.println("Menghitung keliling bangun datar");
+        return 0;
+    }
+    
+}
